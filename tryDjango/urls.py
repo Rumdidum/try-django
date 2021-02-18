95d82ea38685db4ecfc034bd30d693d2da5342fd
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-18 23:50:18 +0100

    templates again

--- tryDjango/urls.py
+++ tryDjango/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', home_view),
     path('home/', home_view),
     path('contact/', contact_view),
     path('about/', about_view),
