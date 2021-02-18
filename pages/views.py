7c624f7f13ee07907c91786c2a6f19446d9e5b09
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-18 23:02:10 +0100

    some django

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact</h1>")
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>About</h1>")
