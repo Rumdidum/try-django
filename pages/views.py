c652824f4ba33124626c3f3cb2de0cb4da925ae3
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-19 00:11:17 +0100

    00:11

--- pages/views.py
+++ pages/views.py
@@ -5,8 +5,9 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "home.html", {})
 
+def about_view(request, *args, **kwargs):
+    my_context = {"Infos": "give me some infos", "random_number": 34}
+    return render(request, "about.html", my_context)
+
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
-
-def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
