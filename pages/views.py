f6a14f927a4ae2b912659233e3b6bfc600c19140
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-18 23:17:35 +0100

    some git

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Contact</h1>")
