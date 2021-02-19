c1d1ed2a20a8ae39ae5f25313f9017339aa2a062
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-19 23:03:38 +0100

    6

--- products/views.py
+++ products/views.py
@@ -3,15 +3,22 @@ from .forms import ProductForm
 from .models import Product
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-        
-    context = {
-        'form': form
-    }
+    print("Was passiert bei GET-----", request.GET)
+    print("Was passiert bei POST----", request.POST)
+    title = request.POST.get('title')
+    context = {}
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
