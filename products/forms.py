d11f2612cfbb0975806881177f3c0781f4bbcd20
Rumdidum <waldemar.rink2021@gmail.com>
2021-02-20 22:53:54 +0100

    sumblime tests

--- products/forms.py
+++ products/forms.py
@@ -11,6 +11,14 @@ class ProductForm(forms.ModelForm):
         ]
 
 class RawProductForm(forms.Form):
-    title       = forms.CharField()
-    description = forms.CharField()
-    price       = forms.DecimalField()
+    title       = forms.CharField(label='')
+    description = forms.CharField(
+                    required=False, 
+                    widget=forms.Textarea(
+                            attrs={
+                                "class": "new class"
+                                "rows": 100
+                            }
+                            )
+                    )
+    price       = forms.DecimalField(initial=199.99)
